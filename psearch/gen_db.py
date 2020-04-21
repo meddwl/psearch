4d16ee12bcdeb0bf395b1241c8d352436870b8e3
Pavel Polishchuk <pavel_polishchuk@ukr.net>
2020-04-21 13:03:22 +0200

    Remove rdkit_factory from args, fix rms filtering, fix argument types and defaults.

--- psearch/gen_db.py
+++ psearch/gen_db.py
@@ -23,16 +23,16 @@ from scripts.read_input import read_input
 from scripts.database import DB
 
 
-def prep_input(fname, rdkit_factory, nconf, nstereo, energy, rms, seed, bin_step, box_mol_names):
+def prep_input(fname, nconf, nstereo, energy, rms, seed, bin_step):
+    box_mol_names = set()
     input_format = 'smi' if fname is None else None
     for mol, mol_name in read_input(fname, input_format=input_format):
         if mol_name in box_mol_names:
-            sys.stderr.write("This {} is meeting the second time in {}. "
-                             "This molecule will be omitted.\n".format(mol_name, os.path.basename(fname)))
+            sys.stderr.write(f'The molecule named {mol_name} meets the second time and will be omitted\n')
             continue
         else:
-            box_mol_names.append(mol_name)
-        yield mol, mol_name, rdkit_factory, nconf, nstereo, energy, rms, seed, bin_step
+            box_mol_names.add(mol_name)
+        yield mol, mol_name, nconf, nstereo, energy, rms, seed, bin_step
 
 
 def map_gen_data(args):
@@ -81,10 +81,9 @@ def remove_confs(mol, energy, rms):
         while any(item[2] < rms for item in rms_list):
             for item in rms_list:
                 if item[2] < rms:
-                    i = item[1]
-                    remove_ids.append(i)
+                    remove_ids.append(item[1])
+                    rms_list = [i for i in rms_list if i[0] != item[1] and i[1] != item[1]]
                     break
-            rms_list = [item for item in rms_list if item[0] != i and item[1] != i]
 
     for cid in set(remove_ids):
         mol.RemoveConformer(cid)
@@ -94,7 +93,7 @@ def remove_confs(mol, energy, rms):
         conf.SetId(i)
 
 
-def gen_data(mol, mol_name, rdkit_factory, nconf, nstereo, energy, rms, seed, bin_step):
+def gen_data(mol, mol_name, nconf, nstereo, energy, rms, seed, bin_step):
     mol_dict, ph_dict, fp_dict = dict(), dict(), dict()
 
     isomers = gen_stereo(mol, nstereo)
@@ -102,7 +101,7 @@ def gen_data(mol, mol_name, rdkit_factory, nconf, nstereo, energy, rms, seed, bi
         mol = gen_conf(mol, nconf, seed)
         remove_confs(mol, energy, rms)
 
-        phs = utils.load_multi_conf_mol(mol, factory=rdkit_factory, bin_step=bin_step)
+        phs = utils.load_multi_conf_mol(mol, bin_step=bin_step)
         mol_dict[i] = mol
         ph_dict[i] = [ph.get_feature_coords() for ph in phs]
         fp_dict[i] = [ph.get_fp() for ph in phs]
@@ -110,41 +109,37 @@ def gen_data(mol, mol_name, rdkit_factory, nconf, nstereo, energy, rms, seed, bi
     return mol_name, mol_dict, ph_dict, fp_dict
 
 
-def create_db(in_fname, out_fname, rdkit_factory, nconf, nstereo, energy, rms, ncpu, bin_step, seed, verbose):
+def create_db(in_fname, out_fname, nconf, nstereo, energy, rms, ncpu, bin_step, seed, verbose):
     if verbose:
         sys.stderr.write('Database creation started\n')
 
     start_time = time.time()
-    box_mol_names = []
 
-    output_file_type = None
-    if out_fname is not None:
-        if os.path.isfile(out_fname):
-            os.remove(out_fname)
+    if os.path.isfile(out_fname):
+        os.remove(out_fname)
 
-        if out_fname.lower().endswith('.sdf.gz'):
-            writer = gzip.open(out_fname, 'a')
-            output_file_type = 'sdf.gz'
-        elif out_fname.lower().endswith('.sdf'):
-            writer = open(out_fname, 'at')
-            output_file_type = 'sdf'
-        elif out_fname.lower().endswith('.pkl'):
-            writer = open(out_fname, 'wb')
-            output_file_type = 'pkl'
-        elif out_fname.lower().endswith(''):
-            db = DB(out_fname)
-            db.write_bin_step(bin_step)
-            output_file_type = 'shelve'
-        else:
-            raise Exception("Wrong output file format. Can be only SDF, SDF.GZ or PKL.")
+    if out_fname.lower().endswith('.sdf.gz'):
+        writer = gzip.open(out_fname, 'a')
+        output_file_type = 'sdf.gz'
+    elif out_fname.lower().endswith('.sdf'):
+        writer = open(out_fname, 'at')
+        output_file_type = 'sdf'
+    elif out_fname.lower().endswith('.pkl'):
+        writer = open(out_fname, 'wb')
+        output_file_type = 'pkl'
+    elif out_fname.lower().endswith('.dat'):
+        db = DB(out_fname)
+        db.write_bin_step(bin_step)
+        output_file_type = 'shelve'
+    else:
+        raise Exception("Wrong output file format. Can be only DAT, SDF, SDF.GZ or PKL.")
 
     nprocess = min(cpu_count(), max(ncpu, 1))
     p = Pool(nprocess)
 
     try:
         for i, (mol_name, mol_dict, ph_dict, fp_dict) in enumerate(
-                p.imap_unordered(map_gen_data, prep_input(in_fname, rdkit_factory,
-                                                          nconf, nstereo, energy, rms, seed, bin_step, box_mol_names),
+                p.imap_unordered(map_gen_data, prep_input(in_fname, nconf, nstereo, energy, rms, seed, bin_step),
                                  chunksize=10), 1):
             if output_file_type == 'shelve':
                 db.write_mol(mol_name, mol_dict)
@@ -157,16 +152,12 @@ def create_db(in_fname, out_fname, rdkit_factory, nconf, nstereo, energy, rms, n
             else:
                 for n, (mol, ph, fp) in enumerate(zip(mol_dict.values(), ph_dict.values(), fp_dict.values())):
                     mol.SetProp("_Name", f'{mol_name}_{n}')
-                    mol.SetProp('pharm', ph)
-                    mol.SetProp('fp', fp)
+                    # mol.SetProp('pharm', '\n'.join(f'{label}\t{x}\t{y}\t{z}' for label, (x, y, z) in ph))
+                    # mol.SetProp('fp', ','.join(map(str, sorted(fp))))
                     string = "$$$$\n".join(Chem.MolToMolBlock(mol, confId=c.GetId()) for c in mol.GetConformers())
                     if string:   # wrong molecules (no valid conformers) will result in empty string
                         string += "$$$$\n"
-                        if out_fname is None:
-                            sys.stdout.write(string)
-                            sys.stdout.flush()
-                        else:
-                            writer.write(string.encode("ascii") if output_file_type == 'sdf.gz' else string)
+                        writer.write(string.encode("ascii") if output_file_type == 'sdf.gz' else string)
             if verbose and i % 10 == 0:
                 sys.stderr.write('\r%i molecules passed/conformers (%is)' % (i, time.time() - start_time))
                 sys.stderr.flush()
@@ -174,8 +165,8 @@ def create_db(in_fname, out_fname, rdkit_factory, nconf, nstereo, energy, rms, n
     finally:
         p.close()
 
-    # if out_fname is not None:
-    #     writer.close()
+    if output_file_type is not 'shelve':
+        writer.close()
 
     if verbose:
         sys.stderr.write("\n")
@@ -183,28 +174,25 @@ def create_db(in_fname, out_fname, rdkit_factory, nconf, nstereo, energy, rms, n
 
 def entry_point():
     parser = argparse.ArgumentParser(description='Generate databased using RDKit.')
-    parser.add_argument('-i', '--in_fname', metavar='input.sdf', required=False, type=str, default=None,
-                        help='input file with structures to generate conformers. Allowed formats SDF or SMILES. '
-                             'if omitted STDIN will be used. STDIN takes only SMILES input (one or two columns).')
-    parser.add_argument('-o', '--out_fname', metavar='output', required=False, type=str, default=None,
-                        help='')
-    parser.add_argument('-f', '--rdkit_factory', metavar='features.fdef', type=str, default=None,
-                        help='text file with definition of pharmacophore features in RDKit format. If file name is not '
-                             'specified the default file from the script dir will be used. This option has '
-                             'a priority over smarts_features.')
-    parser.add_argument('-n', '--nconf', metavar='conf_number', type=int, default=50,
+    parser.add_argument('-i', '--in_fname', metavar='FILENAME', required=True, type=str,
+                        help='input file of SDF or SMILES format (tab-separated).')
+    parser.add_argument('-o', '--out_fname', metavar='FILENAME', required=True, type=str,
+                        help='output database file name. Should have DAT extension. Database will consist of two files '
+                             '.dat and .dir.')
+    parser.add_argument('-b', '--bin_step', type=float, default=1,
+                        help='binning step of pharmacophore model creation. Default: 1.')
+    parser.add_argument('-s', '--nstereo', metavar='INTEGER', type=int, default=5,
+                        help='maximum number of generated stereoisomers per compound (centers with specified '
+                             'stereoconfogurations wil not be altered). Default: 5.')
+    parser.add_argument('-n', '--nconf', metavar='INTEGER', type=int, default=50,
                         help='number of generated conformers. Default: 50.')
-    parser.add_argument('-ns', '--nstereo', metavar='stereo_number', type=int, default=5,
-                        help='number of generated stereoisomers. Default: 3.')
-    parser.add_argument('-e', '--energy_cutoff', metavar='10', type=int, default=10,
-                        help='conformers with energy difference from the lowest found one higher than the specified '
-                             'value will be discarded. Default: 10.')
+    parser.add_argument('-e', '--energy_cutoff', metavar='10', type=float, default=10,
+                        help='conformers with energy difference from the lowest one greater than the specified '
+                             'threshold will be discarded. Default: 10.')
     parser.add_argument('-r', '--rms', metavar='rms_threshold', type=float, default=None,
                         help='only conformers with RMS higher then threshold will be kept. '
                              'Default: None (keep all conformers).')
-    parser.add_argument('-b', '--bin_step', type=int, default=1,
-                        help='binning step. Default: 1.')
-    parser.add_argument('-s', '--seed', metavar='random_seed', type=int, default=-1,
+    parser.add_argument('--seed', metavar='random_seed', type=int, default=-1,
                         help='integer to init random number generator. Default: -1 (means no seed).')
     parser.add_argument('-c', '--ncpu', metavar='cpu_number', type=int, default=1,
                         help='number of cpu to use for calculation. Default: 1.')
@@ -214,7 +202,6 @@ def entry_point():
     args = parser.parse_args()
     create_db(in_fname=args.in_fname,
               out_fname=args.out_fname,
-              rdkit_factory=args.rdkit_factory,
               nconf=args.nconf,
               nstereo=args.nstereo,
               energy=args.energy_cutoff,
@@ -223,6 +210,7 @@ def entry_point():
               ncpu=args.ncpu,
               seed=args.seed,
               verbose=args.verbose)
+
 
 if __name__ == '__main__':
     entry_point()
