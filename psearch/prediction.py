9cfb97b35a739d273ac1bc5bba1289acce6363d8
Alina <alina.kutlushina@pharminnotech.com>
2020-12-17 18:53:25 +0100

    added scoring schems option

--- psearch/prediction.py
+++ psearch/prediction.py
@@ -11,10 +11,12 @@ import pandas as pd
 import argparse
 
 
-def calc_probability(df_vs, df_precision, target_id):
+def calc_probability(df_vs, df_precision, target_id, scoring_scheme):
     df = df_vs.multiply(df_precision['precision'], axis=0)
-
-    df.loc[target_id] = df.mean(axis=0, skipna=True)
+    if scoring_scheme == 'mean':
+        df.loc[target_id] = df.mean(axis=0, skipna=True)
+    elif scoring_scheme == 'max':
+        df.loc[target_id] = df.max(axis=0, skipna=True)
     return round(df.loc[target_id].transpose(), 3)
 
 
@@ -31,7 +33,7 @@ def input_processing(pp_vs, target_id, models_list):
     return df
 
 
-def main(pp_vs, pp_models_stat, pp_output):
+def main(pp_vs, pp_models_stat, scoring_scheme, pp_output):
     df_models_stat = pd.read_csv(pp_models_stat, sep='\t', index_col='model')
     res = pd.DataFrame()
     for target_id in os.listdir(pp_vs):
@@ -39,7 +41,7 @@ def main(pp_vs, pp_models_stat, pp_output):
         df_vs = input_processing(pp_vs, target_id, df_models.index.tolist())
         if df_vs.empty:
             continue
-        df_res = calc_probability(df_vs, df_models, target_id)
+        df_res = calc_probability(df_vs, df_models, target_id, scoring_scheme)
         res = res.merge(df_res, left_index=True, right_index=True, how='outer')
     res.index.name = 'mol_id'
     res.to_csv(pp_output, sep='\t')
@@ -53,11 +55,14 @@ def entry_point():
                         help='path to the virtual screening result')
     parser.add_argument('-p', '--models_stat', metavar='pharmacophores_stat.csv', required=True,
                         help='.csv file with the precision of pharmacophore models')
+    parser.add_argument('-s', '--scoring_scheme', default='mean',
+                        help='two schemes (Max and Mean) of probability calculation for consensus prediction '
+                             'based on individual pharmacophore models were proposed')
     parser.add_argument('-o', '--output', metavar='external_statistics.txt', default=None,
                         help='output text file where will be saved the prediction')
 
     args = parser.parse_args()
-    main(args.path_vs, args.models_stat, args.output)
+    main(args.path_vs, args.models_stat, args.scoring_scheme, args.output)
 
 
 if __name__ == '__main__':
