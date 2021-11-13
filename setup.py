e68764013a735f543e4e09421fb6bea994d6018a
Alina <alina.kutlushina@pharminnotech.com>
2021-11-13 17:14:41 +0100

    changed the pmapper version requirement

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setuptools.setup(
     python_requires='>=3.6',
     extras_require={
     },
-    install_requires=['pmapper>=0.4.0'],
+    install_requires=['pmapper>=0.4.1'],
     entry_points={'console_scripts':
                       ['psearch = psearch.psearch:entry_point',
                        'gen_db = psearch.gen_db:entry_point',
